4598e750c3c1897f8a3a41238a1e8efdbed3ae1a
Robert <majointa@gmail.com>
2021-02-28 17:23:50 +0100

    cms

--- src/pages/special/panel/AdminPanel.js
+++ src/pages/special/panel/AdminPanel.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 const AdminPanel = () => {
-  return <div></div>;
+  return (
+    <section className="saro-panel">
+      <h1>Saro CMS 1.0.0</h1>
+      <div className="panel__items">
+        <div className="panel-box">
+          <h2>News page</h2>
+          <Button>Add new post</Button>
+          <Button>See last posts/edit</Button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AdminPanel;
