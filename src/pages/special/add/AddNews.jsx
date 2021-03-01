c0769ffb44841a02ad93c666483ecff3e30b18ce
michalskirobert <majointa@gmail.com>
2021-03-01 18:45:09 +0100

    fix env

--- src/pages/special/add/AddNews.jsx
+++ src/pages/special/add/AddNews.jsx
@@ -4,10 +4,14 @@ import { Editor } from "@tinymce/tinymce-react";
 const AddNews = () => {
   return (
     <section className="section add-news">
-      <Editor 
- apiKey={`${env.process.REACT_APP_TINY_API_KEY}`}
- init={{ /* your other settings */ }}
-/>
+      <Editor
+        apiKey={`${process.env.REACT_APP_TINY_API_KEY}`}
+        init={
+          {
+            /* your other settings */
+          }
+        }
+      />
     </section>
   );
 };
