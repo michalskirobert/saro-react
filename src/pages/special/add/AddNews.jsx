81ce04801dd6e44f495d48fbbc1ac126bf8c00d9
michalskirobert <majointa@gmail.com>
2021-03-06 22:59:45 +0100

    feat(dev): cms

--- src/pages/special/add/AddNews.jsx
+++ src/pages/special/add/AddNews.jsx
@@ -2,23 +2,46 @@ import React, { useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import { Button } from "react-bootstrap";
 import { firestore } from "../../../components/feature/firebase";
+import CmsAlert from "./../../../components/shared/alerts/CmsAlert";
+import { useSelector, useDispatch } from "react-redux";
+import { cmsActions } from "./../../../_actions";
 
 const AddNews = () => {
   const [query, setQuery] = useState("Simple text");
+  const [title, setTitle] = useState("Title");
+  const isAlert = useSelector((state) => state.CMS.alert);
+  const isLoading = useSelector((state) => state.CMS.isLoading);
+  const dispatch = useDispatch();
+
   const handleEdtiorChange = (e) => {
     setQuery(e.target.getContent());
   };
 
   const handlerSubmit = async (e) => {
     e.preventDefault();
-    await firestore.collection("language").doc("en").collection("blog").add({
-      type: query,
-    });
+    try {
+      dispatch(cmsActions.addNewsReq());
+      await firestore.collection("language").doc("en").collection("news").add({
+        title: title,
+        author: "test",
+        avatarURL: "https://via.placeholder.com/50px",
+        date: new Date().toLocaleString(),
+        content: query,
+        id: 1,
+        imageURL: "https://via.placeholder.com/50px",
+      });
+      dispatch(cmsActions.addNewsSuccess());
+    } catch (error) {
+      dispatch(cmsActions.addNewsFailure());
+    }
+    setQuery("");
   };
 
   return (
     <section className="section add-news">
+      {isAlert && <CmsAlert />}
       <form onSubmit={handlerSubmit}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
         <Editor
           apiKey={`${process.env.REACT_APP_TINY_API_KEY}`}
           initialValue={query}
@@ -42,7 +65,9 @@ const AddNews = () => {
           }}
           onChange={handleEdtiorChange}
         />
-        <Button type="submit">Send</Button>
+        <Button type="submit" disabled={isLoading && true}>
+          Send
+        </Button>
       </form>
     </section>
   );
