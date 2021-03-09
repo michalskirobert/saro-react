2683097168a5eab2d6b0fd1b6a2b5f861b1b6145
michalskirobert <majointa@gmail.com>
2021-03-09 22:55:13 +0100

    fix(master): btoLocalString

--- src/pages/public/home/Blog.jsx
+++ src/pages/public/home/Blog.jsx
@@ -46,7 +46,10 @@ const Blog = () => {
                   <div className="post-info">
                     <div className="post-control">
                       <strong>Published:</strong>
-                      <p style={{ display: "inline" }}> {date}</p>
+                      <p style={{ display: "inline" }}>
+                        {" "}
+                        {new Date(date).toLocaleString() | ""}
+                      </p>
                     </div>
                     <div className="post-control">
                       <strong>Author:</strong>
