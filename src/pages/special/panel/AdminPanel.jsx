ff396038e3d60605a062b63bc93e60926696387f
Robert <majointa@gmail.com>
2021-03-14 13:35:42 +0100

    changes

--- src/pages/special/panel/AdminPanel.jsx
+++ src/pages/special/panel/AdminPanel.jsx
@@ -36,38 +36,51 @@ const AdminPanel = () => {
           </Nav>
         </Tab>
         <Tab eventKey="menagment" title="Menage content">
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Title</th>
-                <th>Published date</th>
-                <th>Author</th>
-                <th>Menagement</th>
-              </tr>
-            </thead>
-            {newsItems.map((post, index) => {
-              const { author, title, date, id, type } = post;
-              return (
-                <tbody key={id}>
+          <Tabs defaultActiveKey="newContent">
+            <Tab eventKey="newContent" title="News management">
+              <Table striped bordered hover>
+                <thead>
                   <tr>
-                    <td>{index}</td>
-                    <td>{title}</td>
-                    <td>{date}</td>
-                    <td>{author}</td>
-                    <td>
-                      <Button variant="primary" onClick={() => handleEdit(id)}>
-                        Edit
-                      </Button>
-                      <Button variant="danger" onClick={() => removeItem(id)}>
-                        Remove
-                      </Button>
-                    </td>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Published date</th>
+                    <th>Author</th>
+                    <th>Menagement</th>
                   </tr>
-                </tbody>
-              );
-            })}
-          </Table>
+                </thead>
+                {newsItems.map((post, index) => {
+                  const { author, title, date, id, type } = post;
+                  return (
+                    <tbody key={id}>
+                      <tr>
+                        <td>{index}</td>
+                        <td>{title}</td>
+                        <td>{date}</td>
+                        <td>{author}</td>
+                        <td>
+                          <Button
+                            variant="primary"
+                            onClick={() => handleEdit(id)}
+                          >
+                            Edit
+                          </Button>
+                          <Button
+                            variant="danger"
+                            onClick={() => removeItem(id)}
+                          >
+                            Remove
+                          </Button>
+                        </td>
+                      </tr>
+                    </tbody>
+                  );
+                })}
+              </Table>
+            </Tab>
+            <Tab eventKey="eventsContent" title="Events menadżerrrooo">
+              <p>dupa</p>
+            </Tab>
+          </Tabs>
         </Tab>
         <Tab eventKey="menagmentOfCrew" title="Menage your profile" disabled>
           2
