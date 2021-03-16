ca74fd03359ccc7743a743d4d746137109855f03
Magda <jaskowska.magda@gmail.com>
2021-03-16 15:18:53 +0000

    feat(#001): toggle inner content - works

--- src/components/layout/nav/NavMenu.jsx
+++ src/components/layout/nav/NavMenu.jsx
@@ -119,11 +119,8 @@ const nav = [
 const NavMenu = () => {
   const linksContainerRef = useRef(null);
   const linksRef = useRef(null);
-  const dispatch = useDispatch();
   const isNavOpen = useSelector((state) => state.isNavOpen);
-  const [isInnerOpen, setIsInnerOpen] = useState(false);
-  const contentRef = useRef(null);
-  const contentContainerRef = useRef(null);
+  const [selected, setSelected] = useState(null);
   const user = useSelector((state) => state.currentUser);
 
   useEffect(() => {
@@ -135,19 +132,16 @@ const NavMenu = () => {
     }
   }, [isNavOpen]);
 
-  useEffect(() => {
-    if (isInnerOpen) {
-      contentContainerRef.current.style.display = "block";
-    } else {
-      contentContainerRef.current.style.display = "none";
-    }
-  }, [isInnerOpen]);
-
   const publicMap = nav.filter((item) => {
     return item.isLogged === false;
   });
-  const toggleInnerMenu = () => {
-    setIsInnerOpen(!isInnerOpen);
+
+  const toggleInnerMenu = (index) => {
+    if (selected === index) {
+      setSelected(null);
+    } else {
+      setSelected(index);
+    }
   };
 
   // let navData = user.isLogged ? loggedMap : nav;
@@ -156,7 +150,6 @@ const NavMenu = () => {
   return (
     <div
       className={`nav-container ${isNavOpen && "active"}`}
-      // className="nav-container active"
       ref={linksContainerRef}
     >
       <ul className="nav-links" ref={linksRef}>
@@ -166,30 +159,32 @@ const NavMenu = () => {
             <li key={index} className="nav-link">
               {content ? (
                 <>
-                  <button onClick={toggleInnerMenu}>
+                  <button
+                    onClick={() => {
+                      toggleInnerMenu(index);
+                    }}
+                  >
                     <FaAngleLeft className="icon" />
                     {title}
                   </button>
 
                   <div
-                    ref={contentContainerRef}
-                    style={{ display: "none" }}
-                    className={`nav-inner-container`}
+                    className={
+                      selected === index
+                        ? "nav-inner-container open"
+                        : "nav-inner-container"
+                    }
                   >
-                    {content?.map((link, index) => {
-                      const { title, path } = link;
-                      return (
-                        <ul
-                          className="nav-links-inner"
-                          ref={contentRef}
-                          key={index}
-                        >
-                          <li className="nav-link-inner">
+                    <ul className="nav-links-inner">
+                      {content?.map((link, index) => {
+                        const { title, path } = link;
+                        return (
+                          <li key={index} className="nav-link-inner">
                             <Link to={path}>{title}</Link>
                           </li>
-                        </ul>
-                      );
-                    })}
+                        );
+                      })}
+                    </ul>
                   </div>
                 </>
               ) : (
