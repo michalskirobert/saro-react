9851b55bd81c48cad5d9bdd5168f87ce8d7e021c
Magda <jaskowska.magda@gmail.com>
2021-03-17 18:01:54 +0000

    feat(#006): header style

--- src/components/layout/nav/Logo.jsx
+++ src/components/layout/nav/Logo.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "./../../../assets/images/components/nav/logo.svg";
 
 const Logo = () => {
   return (
     <Link to="/">
       <div className="logo">
-        <img src={logo} alt="SARO.website" />
-        <p className="logo-title">iSaro</p>
+        <p className="logo-title">Saro</p>
       </div>
     </Link>
   );
