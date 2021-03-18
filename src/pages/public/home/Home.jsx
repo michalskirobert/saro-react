10f0a508ecfe16905af357a3718dadf4b3f8563c
Maciej <you@example.com>
2021-03-18 20:05:44 +0000

    feat(#004): add section two

--- src/pages/public/home/Home.jsx
+++ src/pages/public/home/Home.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Hero from "../../../components/layout/hero/Main";
 import News from "./News";
 import Blog from "./Blog";
-import Event from "./Events";
+import Introduction from "./Introduction";
 
 const Home = () => {
   return (
     <main>
       <Hero />
-      <Event />
+      <Introduction />
       <News />
       <Blog />
     </main>
