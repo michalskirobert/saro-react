95afecc73ac2af08e2916034968ca9596115c341
Robert <majointa@gmail.com>
2021-03-28 14:56:44 +0200

    pair programming

--- src/pages/special/add/validation.js
+++ src/pages/special/add/validation.js
@@ -6,9 +6,9 @@ export const addValidationScheme = yup.object().shape({
   [FORMIK_HELPER.TITLE]: yup
     .string()
     .min(4, "Min 4 characters.")
-    .max(15, "Ma 15 characters.")
+    .max(15, "Max 15 characters.")
     .required("Title is required."),
-  [FORMIK_HELPER.CATEGORY]: yup.string().required("Required"),
+  [FORMIK_HELPER.CATEGORY]: yup.string().required("Category is required."),
   [FORMIK_HELPER.CREW]: yup.string().required(""),
   [FORMIK_HELPER.LANGUAGE]: yup.string().required(""),
   [FORMIK_HELPER.EDITOR]: yup.string().required(""),
