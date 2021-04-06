ca87760ea44fb2d0337f81cc17fc680391c81da8
Maciej Rapacki <mac.rapacki@gmail.com>
2021-04-06 19:06:11 +0100

    feat(Maciej) fetch data from database to POlishLearning section

--- src/pages/public/home/PolishLearning.jsx
+++ src/pages/public/home/PolishLearning.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const PolsihLearning = () => {
-  const heroInfoData = {
-    title: "Polish learning online",
-    subtitle:
-      "A customised online course design for you. W Available 24 hours a day. 7 days a week A community to help you find a partner Related content to help you explore not only the language but also the Polish culture",
-  };
-
+  const sectionData = useSelector((state) => state.database.init.pages);
   return (
     <section className="section__Polish__Learning">
       <div className="polish__Learning__info">
-        <h2 className="title">{heroInfoData.title}</h2>
-        <p className="subtitle">{heroInfoData.subtitle}</p>
-        <button className="btn find-out-btn">Find out more</button>
+        <h2 className="title">
+          {sectionData ? sectionData.homepage[0].sections[0].header : null}
+        </h2>
+        <p className="subtitle">
+          {sectionData ? sectionData.homepage[0].sections[0].details : null}
+        </p>
+        <button className="btn find-out-btn">
+          {" "}
+          {sectionData ? sectionData.homepage[0].sections[0].linkTitle : null}
+        </button>
       </div>
     </section>
   );
