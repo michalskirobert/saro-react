e470dcf2669828b24d9e9d3eb56fc86ff5ba519f
Magda <jaskowska.magda@gmail.com>
2021-04-19 20:08:39 +0100

    fix(magda) new filter cms nav data

--- src/components/layout/nav/NavMenu.jsx
+++ src/components/layout/nav/NavMenu.jsx
@@ -7,221 +7,28 @@ import { FaAngleLeft } from "react-icons/fa";
 
 import * as S from "./style";
 
-const nav1 = [
-  {
-    classLink: "",
-    content: [
-      {
-        path: "/panel",
-        title: "Admin panel",
-      },
-      {
-        status: [40, 50, 60],
-        subcontent: [
-          {
-            path: "/panel/add/events",
-            title: "Events",
-          },
-          {
-            path: "/panel/add/news",
-            title: "News",
-          },
-          {
-            path: "/panel/add/article",
-            title: "Article",
-          },
-        ],
-        title: "Add",
-      },
-      {
-        status: [40, 50, 60],
-        subcontent: [
-          {
-            path: "/panel/manage/events",
-            title: "Events",
-          },
-          {
-            path: "/panel/manage/news",
-            title: "News",
-          },
-          {
-            path: "/panel/manage/article",
-            title: "Article",
-          },
-        ],
-        title: "Managment",
-      },
-      {
-        subcontent: [
-          {
-            title: "General",
-          },
-          {
-            path: "/panel/translate/footer",
-            title: "Footer",
-          },
-        ],
-        title: "Translations",
-      },
-    ],
-    isLogged: true,
-    status: [30, 40, 50, 60],
-    title: "CMS SARO",
-  },
-  {
-    classLink: "",
-    isLogged: false,
-    path: "/",
-    status: [0],
-    title: "Home",
-  },
-  {
-    classLink: "",
-    content: [
-      {
-        path: "/lessons",
-        title: "Begginer",
-      },
-      {
-        path: "lessons/",
-        title: "Elementary",
-      },
-      {
-        path: "/lessons",
-        title: "Pre-intermediate",
-      },
-      {
-        path: "/lessons",
-        title: "Low Intermediate",
-      },
-      {
-        path: "/lessons",
-        title: "Intermediate",
-      },
-      {
-        path: "/lessons",
-        title: "Upper Intermediate",
-      },
-      {
-        path: "/lessons",
-        title: "Pre-advanced",
-      },
-      {
-        path: "/lessons",
-        title: "Advanced",
-      },
-      {
-        path: "/lessons",
-        title: "Very Advanced",
-      },
-    ],
-    isLogged: false,
-    path: "/lessons",
-    status: [0],
-    title: "Lessons",
-  },
-  {
-    classLink: "",
-    isLogged: false,
-    path: "/tests",
-    status: [0],
-    title: "Tests",
-  },
-  {
-    classLink: "",
-    content: [
-      {
-        path: "",
-        title: "Foods",
-      },
-      {
-        path: "",
-        title: "Curiosities",
-      },
-      {
-        path: "",
-        title: "Traditions",
-      },
-      {
-        path: "",
-        title: "Media",
-      },
-      {
-        path: "",
-        title: "Tools",
-      },
-      {
-        path: "",
-        title: "Quiz",
-      },
-      {
-        path: "",
-        title: "Dialogue",
-      },
-      {
-        path: "",
-        title: "Beauty",
-      },
-    ],
-    isLogged: false,
-    path: "/stuff",
-    status: [0],
-    title: "Stuff",
-  },
-  {
-    classLink: "",
-    isLogged: false,
-    path: "/community",
-    status: [0],
-    title: "Community",
-  },
-  {
-    classLink: "",
-    isLogged: false,
-    path: "/about",
-    status: [0],
-    title: "About Us",
-  },
-  {
-    action: 10,
-    classLink: "",
-    isLogged: false,
-    path: "/sign-in",
-    status: [0],
-    title: "Sign In",
-  },
-  {
-    classLink: "",
-    isLogged: true,
-    path: "/sign-in",
-    title: "Sign Out",
-    status: [10, 20, 30, 40, 50, 60],
-  },
-];
-
 const NavMenu = ({ isNavOpen, setIsNavOpen }) => {
-  const user = useSelector((state) => state.currentUser);
+  const user = useSelector((state) => state.currentUser); 
   const nav = useSelector((state) => state.database.init.nav);
 
-  const handleNavClick = () => {
+  const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
   };
 
-  // const filterNavData = () => {
-  //   if (!user.isLogged) {
-  //     return nav.filter(item => !item.isLogged)
-  //   }
-  //   else {
-  //      return nav.filter(item => !item.action && (item?.status?.includes(user?.status) || item?.status.includes(0)) )
-  //   }
-  // }
-
-  // const filteredNavData = filterNavData()
+  const filterNavData = () => {
+    if (user.status === 0 ) {
+      return nav.filter(item => item?.status?.includes(user?.status))
+    }
+    else {
+       return nav.filter(item => !item?.action && item?.status?.includes(user?.status) )
+    }
+  }
+  const filteredNavData = filterNavData()
 
   return (
     <>
       <Accordion className={`nav-container ${isNavOpen && "active"}`}>
-        {nav.map(({ title, path, content }) => {
+        {filteredNavData.map(({ title, path, content }) => {
           return content ? (
             <Card key={title}>
               <Accordion.Toggle eventKey={title} as={Card.Header}>
@@ -261,7 +68,7 @@ const NavMenu = ({ isNavOpen, setIsNavOpen }) => {
           );
         })}
       </Accordion>
-      {isNavOpen && <S.Overlay onClick={handleNavClick}></S.Overlay>}
+      {isNavOpen && <S.Overlay onClick={toggleNav}></S.Overlay>}
     </>
   );
 };
