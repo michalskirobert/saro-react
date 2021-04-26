4fedfd04693597b6a1832d2886e7b8d4d635c309
Robert <majointa@gmail.com>
2021-04-26 18:50:46 +0200

    update

---
+++ src/components/shared/custom-table/container.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { DataTypeProvider } from "@devexpress/dx-react-grid";
+
+import { formatDate } from "../custom-date-picker/utils";
+
+const DateFormatter = ({ value }) => (value ? formatDate(value) : "");
+
+export const DateTypeProvider = (props) => (
+  <DataTypeProvider formatterComponent={DateFormatter} {...props} />
+);
---
+++ src/components/shared/custom-table/custom-table-cell/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+import { Table } from "@devexpress/dx-react-grid-bootstrap4";
+
+export const TableRow = styled(Table.Row)`
+  cursor: pointer;
+`;
