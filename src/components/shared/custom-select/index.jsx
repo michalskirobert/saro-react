22a96840e21f77ba2c56e7e3815d2ba8d728db1e
Robert <majointa@gmail.com>
2021-05-04 20:52:03 +0200

    fix select

--- src/components/shared/custom-select/index.jsx
+++ src/components/shared/custom-select/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Select from "react-select";
 
-export const CustomSelect = ({ label, value, placeholder, onChange }) => {
+export const CustomSelect = ({
+  label,
+  value,
+  placeholder,
+  onChange,
+  isDisabled,
+  invalid,
+}) => {
   return (
     <Select
       {...{
-        className: "custom-select",
+        className: `custom-select ${invalid && "invalid"}`,
         id: value,
         name: value,
         styles: { menuPortal: (base) => ({ ...base, zIndex: 9999 }) },
@@ -15,6 +22,7 @@ export const CustomSelect = ({ label, value, placeholder, onChange }) => {
         value: { label, value },
         options: { label, value },
         onChange: (options) => onChange(options),
+        isDisabled,
       }}
     />
   );
