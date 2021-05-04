3c7d693235681831f0b5339523c1bda123131def
Magda <jaskowska.magda@gmail.com>
2021-05-04 15:30:28 +0100

    feat(magda): main panel page nav

--- src/pages/special/panel/AdminPanel.jsx
+++ src/pages/special/panel/AdminPanel.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { Card, CardGroup } from "react-bootstrap";
 
 import { auth } from "@fire";
 import CmsAlert from "@components/shared/alerts/CmsAlert";
+import { DefaultLoader } from "@components/shared/loadings/DefaultLoader";
 
 const AdminPanel = () => {
   const alert = useSelector((state) => state.CMS.alert);
+  const nav = useSelector((state) => state.database?.init?.nav[0]?.content);
+  const isLoading = useSelector((state) => state.currentUser.isLoading);
 
   return (
     <section className="section saro-panel">
@@ -14,10 +19,33 @@ const AdminPanel = () => {
       <h2>
         Welcome
         <span style={{ color: "red" }}>
-          {auth?.currentUser?.displayName ?? "Saro-crew"}
+          {auth?.currentUser?.displayName ?? " Saro-crew"}
         </span>
       </h2>
-      <p>Admin main page</p>
+      {isLoading && <DefaultLoader />}
+      <CardGroup>
+        {nav &&
+          nav.map(({ title, subcontent, path }, index) => {
+            return subcontent ? (
+              <Card key={index}>
+                <Card.Body>
+                  <Card.Title className="title">{title}</Card.Title>
+                  {subcontent.map(({ title, path }) => {
+                    return <Link to={path}>{title}</Link>;
+                  })}
+                </Card.Body>
+              </Card>
+            ) : (
+              <Card key={index}>
+                <Card.Body>
+                  <Card.Title>
+                    <Link to={path}>{title}</Link>
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            );
+          })}
+      </CardGroup>
     </section>
   );
 };
