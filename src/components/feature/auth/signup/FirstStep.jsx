11f4a1636fcc0f5dc58f7b2a172e190aa47d5190
Maciej Rapacki <mac.rapacki@gmail.com>
2021-05-06 17:35:25 +0100

    feat(Maciej) get data from database to sign up - first step

--- src/components/feature/auth/signup/FirstStep.jsx
+++ src/components/feature/auth/signup/FirstStep.jsx
@@ -1,62 +1,38 @@
+import { Form as F } from "react-bootstrap";
+import { useSelector } from "react-redux";
+
+import uuid4 from "react-uuid";
 import { FORM_HELPER } from "./utils";
 
-import { Form as F } from "react-bootstrap";
-
 export function FirstStep({ handleChange, values, errors, touched }) {
+  const firstStepData = useSelector(
+    (state) => state.database.init.auth["sign-up"]?.labels["step-1"]
+  );
+
   return (
     <>
-      <div className="form-control">
-        <label htmlFor="email" className="floatLabel"></label>
-        <input
-          type="email"
-          id="email"
-          value={values[FORM_HELPER.EMAIL]}
-          onChange={handleChange}
-          required
-          placeholder="jane@example.com"
-          tabindex="1"
-        />
-        {errors[FORM_HELPER.EMAIL] || touched[FORM_HELPER.EMAIL] ? (
-          <F.Text className="validation-alert">
-            {errors[FORM_HELPER.EMAIL]}
-          </F.Text>
-        ) : null}
-      </div>
-      <div className="form-control form-on-top">
-        <label htmlFor="password" className="floatLabel"></label>
-        <input
-          type="password"
-          id="password"
-          value={values[FORM_HELPER.PASSWORD]}
-          onChange={handleChange}
-          required
-          placeholder="••••••••••••"
-          tabindex="2"
-        />
-        {errors[FORM_HELPER.PASSWORD] || touched[FORM_HELPER.PASSWORD] ? (
-          <F.Text className="validation-alert">
-            {errors[FORM_HELPER.PASSWORD]}
-          </F.Text>
-        ) : null}
-      </div>
-      <div className="form-control">
-        <label htmlFor="confPassword" className="floatLabel"></label>
-        <input
-          type="password"
-          id="confPassword"
-          value={values[FORM_HELPER.CONF_PASSWORD]}
-          onChange={handleChange}
-          required
-          placeholder="••••••••••••"
-          tabindex="3"
-        />
-        {errors[FORM_HELPER.CONF_PASSWORD] ||
-        touched[FORM_HELPER.CONF_PASSWORD] ? (
-          <F.Text className="validation-alert">
-            {errors[FORM_HELPER.CONF_PASSWORD]}
-          </F.Text>
-        ) : null}
-      </div>
+      {firstStepData?.map((item, index) => {
+        const { label, placeholder, type } = item;
+        return (
+          <div className="form-control" key={uuid4()}>
+            <label htmlFor={label} className="floatLabel"></label>
+            <input
+              type={type}
+              id={label}
+              value={values[label]}
+              onChange={handleChange}
+              required
+              placeholder={placeholder}
+              tabIndex={index}
+            />
+            {errors[FORM_HELPER.EMAIL] || touched[FORM_HELPER.EMAIL] ? (
+              <F.Text className="validation-alert">
+                {errors[FORM_HELPER.EMAIL]}
+              </F.Text>
+            ) : null}
+          </div>
+        );
+      })}
     </>
   );
 }
