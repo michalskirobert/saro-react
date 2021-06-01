155d9d93fc2a3691589b60d9a91569c651150c4d
Maciej Rapacki <mac.rapacki@gmail.com>
2021-06-01 19:08:39 +0100

    fix confirm password validation

--- src/components/feature/auth/signup/validation.ts
+++ src/components/feature/auth/signup/validation.ts
@@ -6,7 +6,7 @@ export const signUpFirstStepValidationScheme = yup.object().shape({
   Password: yup.string().required("it's required").min(8, "Too short"),
   ["Confirm password"]: yup
     .string()
-    .oneOf([yup.ref("password"), null], "Passwords must match")
+    .oneOf([yup.ref("Password"), null], "Passwords must match")
     .required("It's required"),
   // gender: yup.string().required("It's required"),
   // nativeLang: yup.string().required("It's required"),
